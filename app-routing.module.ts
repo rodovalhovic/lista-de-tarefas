b8d655b3c830fc00880eabf7a435c75620ca303e
rodovalhovic <victorialicia22@gmail.com>
2024-04-28 02:12:44 -0300

    rotas

--- app-routing.module.ts
+++ app-routing.module.ts
@@ -1,7 +1,25 @@
+import { CadastrarUsuarioComponent } from './components/cadastrar-usuario/cadastrar-usuario.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListarTarefasComponent } from './components/listar-tarefas/listar-tarefas.component';
+import { LoginComponent } from './components/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full'
+  },
+  { path: 'login',
+    component: LoginComponent,
+  },
+  { path: 'cadastro',
+    component: CadastrarUsuarioComponent
+  },
+  { path: 'lista-de-tarefas',
+    component: ListarTarefasComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
