fd5c7e8e94d679e608e774e023b1291e3b58dcf2
rodovalhovic <victorialicia22@gmail.com>
2024-04-27 22:54:36 -0300

    criando componentes de login e cadastrar usuario

--- app.component.ts
+++ app.component.ts
@@ -1,33 +1,15 @@
-import { Item } from './interface/item';
-import { Component, DoCheck, OnInit } from '@angular/core';
-import { ListaDeCompraService } from './service/lista-de-tarefas.service';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit, DoCheck{
+export class AppComponent implements OnInit{
   title = 'app-lista-de-compras';
-  listaDeCompra! : Array<Item>;
-  itemParaSerEditado! : Item;
 
-  constructor(private listaService: ListaDeCompraService) {}
+  constructor() {}
 
-  ngOnInit(): void{
-    this.listaDeCompra = this.listaService.getListaDeCompra();
-  }
+  ngOnInit(): void{}
 
-  ngDoCheck(): void {
-    this.listaService.atualizarLocalStorageo();
-  }
-
-  editarItem(item: Item){
-    this.itemParaSerEditado = item;
-  }
-
-  deletarItem(id: number){
-    const index = this.listaDeCompra.findIndex ((item) => item.id === id);
-    this.listaDeCompra.splice(index, 1);
-  }
 }
--- app.module.ts
+++ app.module.ts
@@ -7,12 +7,16 @@ import { CadastrarTarefaComponent } from './components/cadastrar-tarefa/cadastra
 import { ListarTarefasComponent } from './components/listar-tarefas/listar-tarefas.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { FormsModule } from '@angular/forms';
+import { LoginComponent } from './components/login/login.component';
+import { CadastrarUsuarioComponent } from './components/cadastrar-usuario/cadastrar-usuario.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CadastrarTarefaComponent,
-    ListarTarefasComponent
+    ListarTarefasComponent,
+    LoginComponent,
+    CadastrarUsuarioComponent
   ],
   imports: [
     BrowserModule,
