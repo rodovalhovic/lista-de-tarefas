49d1fb9ba87fdf69a40a7eeb29dee8640781c2b9
rodovalhovic <victorialicia22@gmail.com>
2024-04-28 01:08:51 -0300

    layout das telas

--- components/cadastrar-tarefa/cadastrar-tarefa.component.ts
+++ components/cadastrar-tarefa/cadastrar-tarefa.component.ts
@@ -1,45 +1,13 @@
-import { Item } from '../../interface/item';
-import { Component, Input, OnInit, OnChanges, SimpleChanges } from '@angular/core';
-import { ListaDeCompraService } from 'src/app/service/lista-de-tarefas.service';
+import { Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-cadastrar-tarefa',
   templateUrl: './cadastrar-tarefa.component.html',
   styleUrls: ['./cadastrar-tarefa.component.scss']
 })
-export class CadastrarTarefaComponent implements OnInit, OnChanges {
+export class CadastrarTarefaComponent implements OnInit {
 
-  @Input() itemQueVaiSerEditado!: Item;
-  editando = false;
-  textoBtn = "Adicionar";
-  valorItem! : string;
+  constructor() { }
 
-  constructor(private listaService: ListaDeCompraService) { }
-
-  ngOnInit(): void {
-  }
-
-  ngOnChanges(changes: SimpleChanges){
-    if(!changes['itemQueVaiSerEditado'].firstChange){
-      this.editando = true;
-      this.textoBtn = "Salvar";
-    }
-  }
-
-  editarItem(){
-    this.listaService.editarItemLista(this.itemQueVaiSerEditado, this.valorItem);
-    this.limparCampo;
-    this.editando = false;
-    this.textoBtn = "Salvar"
-  }
-
-  adicionarItem(){
-    this.listaService.adicionarItemLista(this.valorItem);
-    this.limparCampo();
-  }
-
-  limparCampo(){
-    this.valorItem = '';
-  }
-
+  ngOnInit(): void {}
 }
--- components/listar-tarefas/listar-tarefas.component.ts
+++ components/listar-tarefas/listar-tarefas.component.ts
@@ -1,33 +1,19 @@
-import { Component, OnInit, Input, OnChanges, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 import { faPen, faTrash } from '@fortawesome/free-solid-svg-icons';
-import { Item } from 'src/app/interface/item';
 
 @Component({
   selector: 'app-listar-tarefas',
   templateUrl: './listar-tarefas.component.html',
   styleUrls: ['./listar-tarefas.component.scss']
 })
-export class ListarTarefasComponent implements OnInit, OnChanges {
-
-  @Input() item!: Item;
-  @Output() emitindoParaEditar = new EventEmitter();
-  @Output() emitindoIdParaDeletar = new EventEmitter();
+export class ListarTarefasComponent implements OnInit {
 
   faPen = faPen;
   faTrash = faTrash;
 
   constructor() { }
 
-  ngOnChanges(): void { }
-
-  ngOnInit(): void { }
-
-  editarItem(){
-    this.emitindoParaEditar.emit(this.item);
-  }
-
-  deletarItem(){
-    this.emitindoIdParaDeletar.emit(this.item.id);
+  ngOnInit(): void {
   }
 
 }
